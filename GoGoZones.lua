4bd445fe3a4016e9fe501608f9c65b3d1e6c41f8
Martijn Krijgsman <m.s.krijgsman@gmail.com>
2023-03-22 13:23:37 +0100

    Added zone the Forbidden Reach

--- GoGoZones.lua
+++ GoGoZones.lua
@@ -4300,6 +4300,12 @@ function GoGo_ZoneCheck()
 			GoGo_DebugAddLine("GoGo_ZoneCheck: Setting up for Valdraken")
 		end --if
 		GoGo_Variables.ZoneExclude.CanFly = true
+	elseif GoGo_Variables.Player.MapID == 2151 then -- The Forbidden Reach
+		GoGo_Variables.ZoneExclude.DragonRiding = true
+		if GoGo_Variables.Debug >= 10 then
+			GoGo_DebugAddLine("GoGo_ZoneCheck: Setting up for The Forbidden Reach")
+		end --if
+		GoGo_Variables.ZoneExclude.CanFly = true	
 	elseif GoGo_Variables.Player.MapID == 2118 then
 		if GoGo_Variables.Debug >= 10 then
 			GoGo_DebugAddLine("GoGo_ZoneCheck: The Forbidden Reach - The Old Weyrn Grounds")
